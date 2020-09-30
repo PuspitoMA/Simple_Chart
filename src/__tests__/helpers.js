2dd3a6b51595ee2d506bb853dc1d07e80473d940
PuspitoMA <pupito3099@gmail.com>
2020-09-30 21:29:41 +0700

    progress bug Simple Chart

--- src/__tests__/helpers.js
+++ src/__tests__/helpers.js
@@ -1,4 +1,44 @@
 /*
     Gunakan file ini untuk membuat test case dengan Jest
 */
-
+const {
+    incrementQty,
+    decrementQty,
+    recalculateSubtotal,
+    calculateDiscount,
+    calculatePay
+  } = require("../helpers");
+  
+  test("qty 1 ditambah 1 sama dengan 2", () => {
+    expect(incrementQty(1)).toBe(2);
+  });
+  test("qty 1 ditambah 1 sama dengan 2", () => {
+    expect(incrementQty("1")).toBe(2);
+  });
+  test("qty 2 dikurangi 1 sama dengan 1", () => {
+    expect(decrementQty(2)).toBe(1);
+  });
+  test("hitung diskon dengan kode25", () => {
+    expect(calculateDiscount(250000, "KODE25")).toBe(187500);
+  });
+  test("hitung kembalian dikurangi subtotal", () => {
+    expect(calculatePay(100000, 75000)).toBe(25000);
+  });
+  
+  
+  
+  /*test("hitung diskon dengan kode", () => {
+    expect(calculateDiscount(250000, "Hello")).toBe(200000);
+  });
+  test("hitung diskon tanpa kode", () => {
+    expect(calculateDiscount(250000, "")).toBe(250000);
+  });
+  test("menghitung subtotal 25000 dengan barang 2 total menjadi 50000", () => {
+    expect(recalculateSubtotal(25000, 2)).toBe(50000);
+  });
+  test("perkalian harga yang isinya string", () => {
+    expect(recalculateSubtotal("25000", "2")).toBe(50000);
+  });
+  test("perkalian harga yang isinya string", () => {
+    expect(recalculateSubtotal("25000aaaaa", "2")).toBe("error");
+  });*/
--- src/app.js
+++ src/app.js
@@ -1,16 +1,38 @@
-import { 
-  incrementQty, 
-  decrementQty
-} from './helpers.js';
+import {
+  incrementQty,
+  decrementQty,
+  recalculateSubtotal,
+  calculateDiscount,
+  calculatePay
+} from "./helpers.js";
 
-const incrButton = document.querySelector('#incr');
-const decrButton = document.querySelector('#decr');
-const qtyInput = document.querySelector('#qty');
+const incrButton = document.querySelector("#incr");
+const decrButton = document.querySelector("#decr");
+const qtyInput = document.querySelector("#qty");
+const subtotalText = document.querySelector("#subtotal");
+const price = document.querySelector("#price");
+const discountCode = document.querySelector("#code");
+const confirmButton = document.querySelector("#confirmButton");
+const backbutton = document.querySelector("#backbutton");
+const moneyText = document.querySelector("#money");
 
-incrButton.addEventListener('click', () => {
+incrButton.addEventListener("click", () => {
   qtyInput.value = incrementQty(qtyInput.value);
+  subtotal.textContent = `Rp. ${recalculateSubtotal(price.value, qtyInput.value)}`;
 });
 
-decrButton.addEventListener('click', () => {
+decrButton.addEventListener("click", () => {
   qtyInput.value = decrementQty(qtyInput.value);
+  subtotal.textContent = `Rp. ${recalculateSubtotal(price.value, qtyInput.value)}`;
+});
+
+confirmButton.addEventListener("click", () => {
+  let subtotal = recalculateSubtotal(price.value, qtyInput.value);
+  let total = calculateDiscount(subtotal, discountCode.value);
+  subtotalText.textContent = `Rp. ${total}`;
+});
+
+backbutton.addEventListener("click", () => {
+  let pay = calculatePay(pay.value, subtotal.value);
+  moneyText.textContent = `Rp. ${calculatePay(pay.value, subtotal.value)}`;
 });
